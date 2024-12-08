c6cdeaecf1af6184d11b71fb20eea5207d91d99c
pressxtohonk <pressxtohonk@gmail.com>
2024-12-08 16:42:15 +0800

    Extracted reusable board logic

--- src/Day06.hs
+++ src/Day06.hs
@@ -6,47 +6,10 @@ import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 import qualified PressXToGrids as Grid
+import PressXToBoard
 import PressXToParse
 import PressXToSolve (Solver, runCLI)
 import Text.Parsec
-
--- Domain model
-type Move = (Pos, Dir)
-
-type Pos = Pair Int
-
-data Dir = U | D | L | R deriving (Show, Ord, Eq)
-
-turn :: Move -> Move
-turn (pos, U) = (pos, R)
-turn (pos, R) = (pos, D)
-turn (pos, D) = (pos, L)
-turn (pos, L) = (pos, U)
-
-step :: Move -> Move
-step ((r, c), U) = ((r - 1, c), U)
-step ((r, c), D) = ((r + 1, c), D)
-step ((r, c), L) = ((r, c - 1), L)
-step ((r, c), R) = ((r, c + 1), R)
-
-data Board = Board
-  { nrow :: Int,
-    ncol :: Int,
-    filled :: Set (Int, Int)
-  }
-  deriving (Show)
-
-hasCell :: Board -> Pos -> Bool
-hasCell (Board nrow ncol _) (r, c)
-  | (r < 1) || (nrow < r) = False
-  | (c < 1) || (ncol < c) = False
-  | otherwise = True
-
-filledAt :: Board -> Pos -> Bool
-filledAt board pos = Set.member pos (filled board)
-
-fill :: Board -> Pos -> Board
-fill board pos = board { filled = Set.insert pos (filled board) }
 
 -- optimized board traversal until next object or out of bounds
 jumpWith :: Board -> Move -> Move
