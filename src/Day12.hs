f997183aa6e981f30fab055aaf38a1638c46f112
pressxtohonk <pressxtohonk@gmail.com>
2024-12-12 23:39:42 +0800

    Day 12: part 2

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,14 +1,17 @@
 module Main where
 
-import Control.Monad.State (State, get, modify, execState)
+import Control.Applicative (asum)
+import Control.Monad.State (StateT, State, get, modify, lift, execState, runStateT)
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
-import Data.Maybe (isJust, maybeToList, fromMaybe, catMaybes, mapMaybe)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import PressXToBoard (Board(Board), Move, Dir(R), turn, step)
+import qualified PressXToBoard as Board
 import PressXToGrids (Grid, (?))
 import qualified PressXToGrids as Grid
 import PressXToParse
 import PressXToSolve (Solver, runCLI)
-import Data.List (uncons)
 
 -- Labels each grid cell with the number of differing neighbours
 numAdjDiff :: Eq a => [[a]] -> [[Int]]
@@ -32,15 +35,21 @@ diff = ((\x -> if x then 1 else 0) <$>) . go Nothing
     go prev (x:xs) = let curr = Just x in (prev /= curr) : go curr xs
 
 regions :: Grid Char -> Grid Int
-regions grid = [[regionMap ! (r-1, c-1) | c <- [1..ncol]] | r <- [1..nrow]]
+regions grid = [[region ! (r-1, c-1) | c <- [1..ncol]] | r <- [1..nrow]]
+  where
+    nrow = Grid.nrow grid
+    ncol = Grid.ncol grid
+    region = regionMap grid
+
+type RegionState = State (Map (Pair Int) Int)
+
+regionMap :: Grid Char -> Map (Pair Int) Int
+regionMap grid = execState update Map.empty
   where
     nrow = Grid.nrow grid
     ncol = Grid.ncol grid
     coords = [(r-1, c-1) | r <- [1..nrow], c <- [1..ncol]]
     update = traverse (uncurry $ fill grid) (zip [0..] coords)
-    regionMap = execState update Map.empty
-
-type RegionState = State (Map (Pair Int) Int)
 
 fill :: Grid Char -> Int -> Pair Int -> RegionState ()
 fill grid region pos = do
@@ -56,6 +65,51 @@ fill grid region pos = do
 adj :: Pair Int -> [Pair Int]
 adj (r, c) = [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]
 
+numSides :: Grid Char -> Map Int Int
+numSides grid = Map.map numTurns boards
+  where
+    boards :: Map Int Board
+    boards = Map.map (Board 0 0) 
+           . Map.fromListWith Set.union
+           . map (\(pos, region) -> (region, Set.singleton pos))
+           $ Map.toList (regionMap grid)
+
+numTurns :: Board -> Int
+numTurns board = go Set.empty moves
+  where
+    moves = (, R) <$> Set.toList (Board.filled board)
+    go seen [] = 0
+    go seen (x:xs) = case walk board x seen of
+      Nothing -> go seen xs
+      Just (n, seen') -> n + go seen' xs
+
+type WalkState = StateT (Set Move) Maybe
+
+walk :: Board -> Move -> Set Move -> Maybe (Int, Set Move)
+walk board move@(pos, _) = runStateT (go move)
+  where
+    turnL = turn . turn . turn
+    turnR = turn
+
+    inBounds = Board.filledAt board . fst
+    leftWall = step . turnL
+    onPath move = inBounds move && (not . inBounds . step . turnL $ move)
+
+    go :: Move -> WalkState Int
+    go move = do
+      seen <- get
+      if Set.member move seen 
+        then return 0
+        else if onPath move 
+          then modify (Set.insert move) >> asum nexts
+          else lift Nothing
+      where
+        nexts =
+          [ (+1) <$> go (step . turnL . step $ move)
+          , go (step move)
+          , (+1) <$> go (turnR move)
+          ]
+
 solve1 :: Solver
 solve1 input = show $ sum costs
 -- solve1 input = unlines ["", unlines plots, showGrid labels, showGrid counts]
@@ -68,7 +122,13 @@ solve1 input = show $ sum costs
     costs = Map.unionWith (*) areas dists
 
 solve2 :: Solver
-solve2 = show
+solve2 input = show $ sum costs
+  where
+    plots = Grid.fromLists $ mustParse block input
+    labels = concat (regions plots)
+    areas = Map.fromListWith (+) (map (, 1) labels)
+    dists = numSides plots
+    costs = Map.unionWith (*) areas dists
 
 main :: IO ()
 main = runCLI solve1 solve2
