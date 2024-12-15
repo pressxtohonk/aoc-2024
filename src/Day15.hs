4c3edcfa0b91e4c4fe08d25b9e27cd248722f86c
pressxtohonk <pressxtohonk@gmail.com>
2024-12-15 14:16:50 +0800

    Day 15: part 1

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,10 +1,12 @@
 module Main where
 
-import PressXToBoard (Board, Dir, Pos, Move)
+import qualified Data.Map as Map
+import PressXToBoard (Board, Dir, Pos, Move, (!), (?))
 import qualified PressXToBoard as Board
 import PressXToParse
 import PressXToSolve (Solver, runCLI)
 import Text.Parsec (char, many)
+import Data.Maybe (fromMaybe)
 
 data Block
   = Robot
@@ -41,11 +43,33 @@ puzzleP = do
         board = Board.fromLists cells
     _ -> error $ "error parsing puzzle, expected 2 sections but got " ++ show (length sections)
 
+push :: Move -> Board Block -> Maybe (Board Block)
+push move@(pos, _) board = case board ? pos of
+  Just Wall -> Nothing
+  Just Empty -> Just board
+  Just Robot -> update Robot <$> push move' board
+  Just Box -> update Box <$> push move' board
+  result -> error $ "push got unexpected grid result " ++ show result
+  where
+    move'@(pos', _) = Board.step move
+    update block board = Board.fill (Board.fill board pos Empty) pos' block
+
+pushRobot :: Dir -> Board Block -> Board Block
+pushRobot dir board = fromMaybe board (push (pos, dir) board)
+  where
+    pos = head . Map.keys $ Map.filter (==Robot) (Board.filled board)
+
+gpsCoordSum :: Board Block -> Int
+gpsCoordSum = Map.foldrWithKey combine 0 . Board.filled
+  where
+    combine (r, c) Box acc = 100 * r + c + acc
+    combine _ _ acc = acc
+
 solve1 :: Solver
-solve1 input = show (board, moves)
+solve1 input = show $ gpsCoordSum board'
   where
     (board, moves) = mustParse puzzleP input
-
+    board' = foldr pushRobot board (reverse moves)
 
 solve2 :: Solver
 solve2 = show
