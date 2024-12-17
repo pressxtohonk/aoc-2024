969884a7d44c3fbd871afbb6b80e9fa95a9e0dc1
pressxtohonk <pressxtohonk@gmail.com>
2024-12-17 17:56:08 +0800

    Day 17: part 2

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,8 +1,10 @@
 module Main where
 
-import Control.Monad.Writer
+import Control.Applicative (asum)
+import Control.Monad.State
 import Data.Bits
 import Data.List (intercalate)
+import Data.Maybe (fromJust)
 import PressXToParse
 import PressXToSolve (Solver, runCLI)
 import Text.Parsec (char, eof, lookAhead, many, optional, sepBy, string, try)
@@ -69,6 +71,9 @@ computer = do
 program :: Parser Program
 program = string "Program: " *> many (try opcode) <* int <* optional eol
 
+opCodes :: Parser [Int]
+opCodes = string "Program: " *> int `sepBy` char ','
+
 debugger :: Parser (Computer, Program)
 debugger = do
   c <- computer <* eol
@@ -76,7 +81,7 @@ debugger = do
   eof
   return (c, p)
 
-type OutputW = Writer [Int]
+type WithOutput = State [Int]
 
 val :: Operand -> Computer -> Int
 val (Lit x) = const x
@@ -88,55 +93,89 @@ val Reserved = error "cannot get value for reserved operand"
 incr :: Computer -> Computer
 incr comp = comp {p = p comp + 2}
 
-adv :: Operand -> Computer -> OutputW Computer
+adv :: Operand -> Computer -> WithOutput Computer
 adv op comp = return $ incr comp {a = a comp `div` (0b1 `shift` val op comp)}
 
-bxl :: Operand -> Computer -> OutputW Computer
+bxl :: Operand -> Computer -> WithOutput Computer
 bxl op comp = return $ incr comp {b = b comp `xor` val op comp}
 
-bst :: Operand -> Computer -> OutputW Computer
+bst :: Operand -> Computer -> WithOutput Computer
 bst op comp = return $ incr comp {b = val op comp .&. 0b111}
 
-jnz :: Operand -> Computer -> OutputW Computer
+jnz :: Operand -> Computer -> WithOutput Computer
 jnz op comp
   | a comp == 0 = return $ incr comp
   | otherwise = return $ comp {p = val op comp}
 
-bxc :: Computer -> OutputW Computer
+bxc :: Computer -> WithOutput Computer
 bxc comp = return $ incr comp {b = b comp `xor` c comp}
 
-out :: Operand -> Computer -> OutputW Computer
-out op comp = writer (incr comp, [val op comp .&. 0b111])
+out :: Operand -> Computer -> WithOutput Computer
+out op comp = do
+  modify (val op comp .&. 0b111 :)
+  return $ incr comp
 
-bdv :: Operand -> Computer -> OutputW Computer
+bdv :: Operand -> Computer -> WithOutput Computer
 bdv op comp = return $ incr comp {b = a comp `div` (0b1 `shift` val op comp)}
 
-cdv :: Operand -> Computer -> OutputW Computer
+cdv :: Operand -> Computer -> WithOutput Computer
 cdv op comp = return $ incr comp {c = a comp `div` (0b1 `shift` val op comp)}
 
-run :: Program -> Computer -> OutputW Computer
+runInstruction :: Instruction -> Computer -> WithOutput Computer
+runInstruction ix = case ix of
+  Adv op -> adv op
+  Bxl op -> bxl op
+  Bst op -> bst op
+  Jnz op -> jnz op
+  Bxc -> bxc
+  Out op -> out op
+  Bdv op -> bdv op
+  Cdv op -> cdv op
+
+run :: Program -> Computer -> WithOutput Computer
 run prog comp
-  | p comp >= length prog = return comp
-  | otherwise = update comp >>= run prog
+  | p comp < length prog = update comp >>= run prog
+  | otherwise = return comp
   where
-    update = case prog !! p comp of
-      Adv op -> adv op
-      Bxl op -> bxl op
-      Bst op -> bst op
-      Jnz op -> jnz op
-      Bxc -> bxc
-      Out op -> out op
-      Bdv op -> bdv op
-      Cdv op -> cdv op
+    update = runInstruction (prog !! p comp)
+
+format :: [Int] -> String
+format = intercalate "," . map show . reverse
+
+findSeed :: (Int -> Int) -> [Int] -> Maybe Int
+findSeed next output = asum (go output' 0 <$> [1..7])
+  where
+    output' = reverse output
+    go :: [Int] -> Int -> Int -> Maybe Int
+    go [] acc _ = Just acc
+    go (x:xs) acc n
+      | next acc' == x = asum (go xs acc' <$> [0..7])
+      | otherwise = Nothing
+      where
+        acc' = 8 * acc + n
+
+nextOutput :: Program -> Int -> Int
+nextOutput prog a0 = head $ execState s []
+  where
+    comp = Computer 0 a0 0 0
+    s = foldl (>>=) (return comp) (runInstruction <$> prog)
+
+alternate :: [a] -> [a]
+alternate (x:_:xs) = x : alternate xs
+alternate xs = xs
 
 solve1 :: Solver
-solve1 input = unlines ["", show comp, show prog, output]
+solve1 input = format $ execState (run prog comp) []
   where
     (comp, prog) = mustParse debugger input
-    output = intercalate "," . map show $ execWriter (run prog comp)
 
 solve2 :: Solver
-solve2 = show
+solve2 input = show $ fromJust (seed target)
+  where
+    (_, prog) = mustParse debugger input
+    next = nextOutput (alternate prog)
+    seed = findSeed next
+    target = mustParse (block >> eol >> opCodes) input
 
 main :: IO ()
 main = runCLI solve1 solve2
