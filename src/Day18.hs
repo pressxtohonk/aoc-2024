148e54dce7ecd29f8fd6a4964a3d036c210d759a
pressxtohonk <pressxtohonk@gmail.com>
2024-12-18 20:12:39 +0800

    Day 18: updated solns to use shared BFS algo

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,10 +1,6 @@
 module Main where
 
-import Control.Monad (guard)
-import Control.Monad.State
 import qualified Data.Map as Map
-import qualified Data.Set as Set
-import qualified Data.Tree as Tree
 import qualified PressXToBoard as Board
 import PressXToParse
 import PressXToSolve (Solver, runCLI)
@@ -13,66 +9,36 @@ import Text.Parsec (char)
 queueP :: Parser [Pair Int]
 queueP = linesOf $ (,) <$> int <* char ',' <*> int
 
-type Seed = (Board.Board (), Path)
-
-type Path = [Board.Pos]
-
-type WithSeen = State (Set.Set Board.Pos)
-
-explore :: Board.Board () -> Path -> WithSeen (Path, [Path])
-explore board path = case path of
-  (pos : _) -> do
-    seen <- get
-    let peers = filter (`Set.notMember` seen) (Board.peers board pos)
-    put (foldr Set.insert seen (pos : peers))
-    return (path, [peer : path | peer <- peers])
-  _ -> return ([], [])
-
-shortestPathsTo :: Board.Pos -> Tree.Tree Path -> [Path]
-shortestPathsTo target tree = go Set.empty [tree]
-  where
-    go seen forest
-      | null forest = []
-      | not (null soln) = soln
-      | otherwise = go seen' forest'
-      where
-        paths = Tree.rootLabel <$> forest
-        soln = filter ((== target) . head) paths
-        seen' = foldr (Set.insert . head) seen paths
-        forest' = do
-          Tree.Node (h : _) subForest <- forest
-          guard (Set.notMember h seen)
-          subForest
-
 -- NOTE: Binary search would be faster, but our search space only has 3450 elements
 firstFatalByte :: Board.Board () -> [Board.Pos] -> Board.Pos
 firstFatalByte board [] = error "no fatal bytes"
 firstFatalByte board (pos : rest)
-  | null (shortestPathsTo (nrow - 1, ncol - 1) tree) = pos
+  | null soln = pos
   | otherwise = firstFatalByte board' rest
   where
-    board' = Board.fill board pos ()
     nrow = Board.nrow board
     ncol = Board.ncol board
-    tree = evalState (Tree.unfoldTreeM_BF (explore board') [(0, 0)]) Set.empty
+    board' = Board.fill board pos ()
+    soln = Board.shortestPath board' (0, 0) (nrow - 1, ncol - 1)
 
 solve1 :: Solver
-solve1 input = show $ minimum (length <$> shortestPathsTo (nrow - 1, ncol - 1) tree')
+solve1 input = show $ minimum (length <$> paths) - 1 -- count moves, not nodes
   where
-    nrow = 71
-    ncol = 71
-    nbits = 1024
-    board = Board.Board nrow ncol (Map.fromList $ map (,()) (take nbits queue))
+    nrow = 71 -- 7 for tests, 71 for solve
+    ncol = 71 -- 7 for tests, 71 for solve
+    nbits = 1024  -- 12 for tests, 1024 for solve
     queue = mustParse queueP input
-    tree' = evalState (Tree.unfoldTreeM_BF (explore board) [(0, 0)]) Set.empty
+    board = Board.Board nrow ncol (Map.fromList $ map (,()) (take nbits queue))
+    paths = Board.shortestPath board (0, 0) (nrow - 1, ncol - 1)
 
 solve2 :: Solver
-solve2 input = show $ firstFatalByte board queue
+solve2 input = show r ++ "," ++ show c
   where
-    nrow = 71
-    ncol = 71
+    nrow = 71 -- 7 for tests, 71 for solve
+    ncol = 71 -- 7 for tests, 71 for solve
     board = Board.Board nrow ncol Map.empty
     queue = mustParse queueP input
+    (r, c) = firstFatalByte board queue
 
 main :: IO ()
 main = runCLI solve1 solve2
