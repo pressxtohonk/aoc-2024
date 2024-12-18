df14d344603360ad4f812dfb1183bc4266cb23c1
pressxtohonk <pressxtohonk@gmail.com>
2024-12-18 23:04:54 +0800

    Day 16: fixed part 1 soln (broken by changes needed for part 2)

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,16 +1,16 @@
 module Main where
 
-import Data.Set (Set)
-import qualified Data.Set as Set
+import Data.List (nub)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Maybe (fromJust, mapMaybe)
+import Data.Set (Set)
+import qualified Data.Set as Set
 import PressXToBoard (Board, (?))
 import qualified PressXToBoard as Board
 import PressXToParse
 import PressXToSolve (Solver, runCLI)
 import Text.Parsec (char, manyTill)
-import Data.Maybe (fromJust, mapMaybe)
-import Data.List (nub)
 
 data Tile
   = Start
@@ -20,12 +20,13 @@ data Tile
   deriving (Show, Eq)
 
 tile :: Parser Tile
-tile = anyOf
-  [ Start <$ char 'S'
-  , Floor <$ char '.'
-  , Wall <$ char '#'
-  , End <$ char 'E'
-  ]
+tile =
+  anyOf
+    [ Start <$ char 'S',
+      Floor <$ char '.',
+      Wall <$ char '#',
+      End <$ char 'E'
+    ]
 
 maze :: Parser (Board Tile)
 maze = Board.fromLists <$> ((tile `manyTill` eol) `manyTill` eol)
@@ -36,45 +37,16 @@ extend :: (Int -> Int) -> (Board.Move -> Board.Move) -> Branch -> Branch
 extend f g (cost, _, move) = (f cost, move, g move)
 
 step :: Branch -> Branch
-step = extend (+1) Board.step
+step = extend (+ 1) Board.step
 
 spin :: Branch -> Branch
-spin = extend (+2000) (Board.turnR . Board.turnR)
+spin = extend (+ 2000) (Board.turnR . Board.turnR)
 
 turnL :: Branch -> Branch
-turnL = extend (+1000) Board.turnL
+turnL = extend (+ 1000) Board.turnL
 
 turnR :: Branch -> Branch
-turnR = extend (+1000) Board.turnR
-
--- Optimized to never walk over the same tile twice
-shortestPath :: Board Tile -> Board.Move -> Maybe Int
-shortestPath board start = go board branches
-  where
-    board' = Board.fill board (Board.pos start) Wall
-    branches = Set.fromList $ map ($ (0, start, start)) [step, turnL, turnR, spin]
-    go :: Board Tile -> Set Branch -> Maybe Int
-    go board paths = do
-      branch@(cost, _, move@(pos, _)) <- Set.lookupMin paths
-      let
-        board' = Board.fill board pos Wall
-        paths' = Set.deleteMin paths
-        updates =
-          [ Set.insert (step branch)
-          , Set.insert (turnL branch)
-          , Set.insert (turnR branch)
-          ]
-      tile <- board ? pos
-      case tile of
-        End -> Just cost
-        Floor -> go board' (foldr ($) paths' updates)
-        _ -> go board' paths'
-
-escapeCost :: Board Tile -> Maybe Int
-escapeCost board = shortestPath board (startPos, east)
-  where
-    startPos = head . Map.keys $ Map.filter (==Start) (Board.filled board)
-    east = Board.R
+turnR = extend (+ 1000) Board.turnR
 
 type PrevMovesByCost = Map Board.Move [(Int, Board.Move)]
 
@@ -82,7 +54,7 @@ type PrevMovesByCost = Map Board.Move [(Int, Board.Move)]
 escape :: Board Tile -> Maybe PrevMovesByCost
 escape board = go board (Set.singleton tmpBranch) Map.empty
   where
-    startPos = head . Map.keys $ Map.filter (==Start) (Board.filled board)
+    startPos = head . Map.keys $ Map.filter (== Start) (Board.filled board)
     startDir = Board.R
     startMove = (startPos, startDir)
     board' = Board.fill board startPos Wall
@@ -93,11 +65,10 @@ escape board = go board (Set.singleton tmpBranch) Map.empty
       | null branches = Just acc
       | otherwise = do
           branch@(cost, prev, move@(pos, dir)) <- Set.lookupMin branches
-          let
-            acc' = Map.insertWith (++) move [(cost, prev)] acc
-            board' = Board.fill board pos Wall
-            branches' = Set.deleteMin branches
-            updates = [Set.insert (f branch) | f <- [step, turnL, turnR, spin]]
+          let acc' = Map.insertWith (++) move [(cost, prev)] acc
+              board' = Board.fill board pos Wall
+              branches' = Set.deleteMin branches
+              updates = [Set.insert (f branch) | f <- [step, turnL, turnR, spin]]
           tile <- board ? pos
           if Map.member move acc
             then go board branches' acc'
@@ -110,7 +81,7 @@ escape board = go board (Set.singleton tmpBranch) Map.empty
 getPaths :: Board Tile -> PrevMovesByCost -> [[Board.Move]]
 getPaths board soln = concatMap (go [[]]) [move | (c, move) <- endSolns, c == minCost]
   where
-    endPos = head . Map.keys $ Map.filter (==End) (Board.filled board)
+    endPos = head . Map.keys $ Map.filter (== End) (Board.filled board)
     endMoves = (endPos,) <$> [Board.U, Board.D, Board.L, Board.R]
     endSolns = concat $ mapMaybe (($ soln) . Map.lookup) endMoves
     minCost = minimum (fst <$> endSolns)
@@ -118,24 +89,34 @@ getPaths board soln = concatMap (go [[]]) [move | (c, move) <- endSolns, c == mi
     go :: [[Board.Move]] -> Board.Move -> [[Board.Move]]
     go acc move@(pos, dir) = case (board ? pos, Map.lookup move soln) of
       (Just Wall, _) -> error $ "unexpected wall in solution @ " ++ show pos
-      (Just Start, _) -> map (move:) acc
+      (Just Start, _) -> map (move :) acc
       (Just _, Just prevs) -> concatMap (go acc') nexts
         where
-          acc' = map (move:) acc
+          acc' = map (move :) acc
           minCost = minimum (fst <$> prevs)
           nexts = [move | (c, move) <- prevs, c == minCost]
       (Nothing, _) -> error $ "out of bounds @ " ++ show pos
       (_, Nothing) -> []
 
+getCosts :: Board Tile -> PrevMovesByCost -> [Int]
+getCosts board soln = fst <$> endSolns
+  where
+    endPos = head . Map.keys $ Map.filter (== End) (Board.filled board)
+    endMoves = (endPos,) <$> [Board.U, Board.D, Board.L, Board.R]
+    endSolns = concat $ mapMaybe (($ soln) . Map.lookup) endMoves
+
+costs :: Board Tile -> [Int]
+costs board = getCosts board (fromJust (escape board))
+
 paths :: Board Tile -> [[Board.Move]]
 paths board = getPaths board (fromJust (escape board))
 
 solve1 :: Solver
-solve1 = show . fromJust . escapeCost . mustParse maze
+solve1 = show . minimum . costs . mustParse maze
 
 -- TODO: tidy this up! it's horrendous,,,
 solve2 :: Solver
-solve2 = show . (+1) . length . nub . map fst . concat . paths . mustParse maze
+solve2 = show . (+ 1) . length . nub . map fst . concat . paths . mustParse maze
 
 main :: IO ()
 main = runCLI solve1 solve2
