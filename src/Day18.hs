31308e4daeecb66eb8562e12bb05f1f695dbd363
pressxtohonk <pressxtohonk@gmail.com>
2024-12-18 20:52:57 +0800

    Day 18: optimized part 2 soln with binary search

--- src/Day18.hs
+++ src/Day18.hs
@@ -9,24 +9,35 @@ import Text.Parsec (char)
 queueP :: Parser [Pair Int]
 queueP = linesOf $ (,) <$> int <* char ',' <*> int
 
--- NOTE: Binary search would be faster, but our search space only has 3450 elements
 firstFatalByte :: Board.Board () -> [Board.Pos] -> Board.Pos
-firstFatalByte board [] = error "no fatal bytes"
-firstFatalByte board (pos : rest)
-  | null soln = pos
-  | otherwise = firstFatalByte board' rest
+firstFatalByte board queue = case binarySearch noSoln 0 (length queue) of
+  Nothing -> error "no fatal bytes"
+  Just (_, i) -> queue !! (i - 1)
   where
-    nrow = Board.nrow board
-    ncol = Board.ncol board
-    board' = Board.fill board pos ()
-    soln = Board.shortestPath board' (0, 0) (nrow - 1, ncol - 1)
+    noSoln nbits = null soln
+      where
+        nrow = Board.nrow board
+        ncol = Board.ncol board
+        board' = Board.Board nrow ncol (Map.fromList $ map (,()) (take nbits queue))
+        soln = Board.shortestPath board' (0, 0) (nrow - 1, ncol - 1)
+
+-- Find the first (i, i+1) in [lb .. ub] such that f <$> [i, i+1] = [False, True]
+binarySearch :: (Int -> Bool) -> Int -> Int -> Maybe (Int, Int)
+binarySearch f lb ub
+  | lb > ub = Nothing
+  | ub - lb == 1 = Just (lb, ub)
+  | otherwise =
+      let mid = (lb + ub) `div` 2
+       in if f mid
+            then binarySearch f lb mid
+            else binarySearch f mid ub
 
 solve1 :: Solver
 solve1 input = show $ minimum (length <$> paths) - 1 -- count moves, not nodes
   where
     nrow = 71 -- 7 for tests, 71 for solve
     ncol = 71 -- 7 for tests, 71 for solve
-    nbits = 1024  -- 12 for tests, 1024 for solve
+    nbits = 1024 -- 12 for tests, 1024 for solve
     queue = mustParse queueP input
     board = Board.Board nrow ncol (Map.fromList $ map (,()) (take nbits queue))
     paths = Board.shortestPath board (0, 0) (nrow - 1, ncol - 1)
