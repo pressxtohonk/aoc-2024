e02133f11e8885e0d79c609fafc66c2eae8dc9c5
pressxtohonk <pressxtohonk@gmail.com>
2024-12-18 23:13:55 +0800

    Day 18: updated solns to use improved bfs algo

--- src/Day18.hs
+++ src/Day18.hs
@@ -19,7 +19,7 @@ firstFatalByte board queue = case binarySearch noSoln 0 (length queue) of
         nrow = Board.nrow board
         ncol = Board.ncol board
         board' = Board.Board nrow ncol (Map.fromList $ map (,()) (take nbits queue))
-        soln = Board.shortestPath board' (0, 0) (nrow - 1, ncol - 1)
+        soln = Board.shortestPaths board' (0, 0) (nrow - 1, ncol - 1)
 
 -- Find the first (i, i+1) in [lb .. ub] such that f <$> [i, i+1] = [False, True]
 binarySearch :: (Int -> Bool) -> Int -> Int -> Maybe (Int, Int)
@@ -33,14 +33,14 @@ binarySearch f lb ub
             else binarySearch f mid ub
 
 solve1 :: Solver
-solve1 input = show $ minimum (length <$> paths) - 1 -- count moves, not nodes
+solve1 input = show $ length (head paths) - 1 -- count moves, not nodes
   where
     nrow = 71 -- 7 for tests, 71 for solve
     ncol = 71 -- 7 for tests, 71 for solve
     nbits = 1024 -- 12 for tests, 1024 for solve
     queue = mustParse queueP input
     board = Board.Board nrow ncol (Map.fromList $ map (,()) (take nbits queue))
-    paths = Board.shortestPath board (0, 0) (nrow - 1, ncol - 1)
+    paths = Board.shortestPaths board (0, 0) (nrow - 1, ncol - 1)
 
 solve2 :: Solver
 solve2 input = show r ++ "," ++ show c
