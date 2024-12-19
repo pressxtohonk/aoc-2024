bb45375ef74144050a72a97b3f91c4d2e28810ee
pressxtohonk <pressxtohonk@gmail.com>
2024-12-19 16:02:14 +0800

    Day 19: part 2

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,7 +1,11 @@
 module Main where
 
 import Control.Applicative (asum)
+import Control.Monad (when)
+import Control.Monad.RWS
 import Data.List (stripPrefix)
+import Data.Map ((!))
+import qualified Data.Map as Map
 import Data.Maybe (mapMaybe)
 import qualified Data.Tree as Tree
 import PressXToParse
@@ -25,6 +29,21 @@ search word (Tree.Node part rest)
       word' <- stripPrefix part word
       (part :) <$> asum (search word' <$> rest)
 
+type Cached a = RWS () [String] (Map.Map String a) a
+
+countSolns :: String -> Tree.Tree String -> Cached Int
+countSolns word (Tree.Node part forest) = case stripPrefix part word of
+  Nothing -> return 0
+  Just rest -> do
+    cache <- get
+    when (Map.notMember rest cache) $ do
+      n <- if rest == "" then return 1 else sum <$> mapM (countSolns rest) forest
+      tell [unwords [show n, "-> cache", show (word, part, rest)]]
+      modify (Map.insert rest n)
+    n <- gets (! rest)
+    tell [unwords [show n, "<- cache", show (word, part, rest)]]
+    return n
+
 solve1 :: Solver
 solve1 input = show $ length (mapMaybe (`search` tree) designs)
   where
@@ -32,7 +51,12 @@ solve1 input = show $ length (mapMaybe (`search` tree) designs)
     tree = Tree.unfoldTree (generate patterns) ""
 
 solve2 :: Solver
-solve2 = show
+solve2 input = show $ sum counts
+  where
+    (patterns, designs) = mustParse inputP input
+    tree = Tree.unfoldTree (generate patterns) ""
+    actions = mapM (`countSolns` tree) designs
+    (counts, cache, log) = runRWS actions () Map.empty
 
 main :: IO ()
 main = runCLI solve1 solve2
