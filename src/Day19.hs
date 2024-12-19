72499d0e23c0e2bc019dc05b29b07ab1f3c559dc
pressxtohonk <pressxtohonk@gmail.com>
2024-12-20 02:33:11 +0800

    Day 19: added improved DP solution

--- src/Day19.hs
+++ src/Day19.hs
@@ -3,7 +3,7 @@ module Main where
 import Control.Applicative (asum)
 import Control.Monad (when)
 import Control.Monad.RWS
-import Data.List (stripPrefix)
+import Data.List (stripPrefix, tails)
 import Data.Map ((!))
 import qualified Data.Map as Map
 import Data.Maybe (mapMaybe)
@@ -44,6 +44,29 @@ countSolns word (Tree.Node part forest) = case stripPrefix part word of
     tell [unwords [show n, "<- cache", show (word, part, rest)]]
     return n
 
+-- Tracks the number of ways each word can be composed from fragments
+type Cache = Map.Map String Int
+
+cache :: Cache
+cache = Map.singleton "" 1
+
+-- DP solution with more compact caching than the trie-based soln above
+countArrangements :: [String] -> String -> Int
+countArrangements parts word =
+  let counts = foldr (countCombiOf parts) cache (tails word)
+   in Map.findWithDefault 0 word counts
+
+-- Updates a cache for `word` with the number of ways it composes from:
+-- a prefix from `parts` followed by a suffix from `cache` keys
+countCombiOf :: [String] -> String -> Cache -> Cache
+countCombiOf parts word cache
+  | Map.notMember word cache && (count > 0) = Map.insert word count cache
+  | otherwise = cache
+  where
+    suffixes = mapMaybe (`stripPrefix` word) parts
+    subCounts = mapMaybe (`Map.lookup` cache) suffixes
+    count = sum subCounts
+
 solve1 :: Solver
 solve1 input = show $ length (mapMaybe (`search` tree) designs)
   where
@@ -58,5 +81,10 @@ solve2 input = show $ sum counts
     actions = mapM (`countSolns` tree) designs
     (counts, cache, log) = runRWS actions () Map.empty
 
+solve2' :: Solver
+solve2' input = show $ foldr ((+) . countArrangements patterns) 0 designs
+  where
+    (patterns, designs) = mustParse inputP input
+
 main :: IO ()
-main = runCLI solve1 solve2
+main = runCLI solve1 solve2'
